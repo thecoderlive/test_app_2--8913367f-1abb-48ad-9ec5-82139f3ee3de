f8958637e6892526f0deb1743dc4f6a84d14eda8
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-03 00:06:21 +0530

    changed home screen label's font size

--- HistoryList.js
+++ HistoryList.js
@@ -44,6 +44,7 @@ const styles = StyleSheet.create({
         "marginTop": 5,
         "fontWeight": "400",
         "marginHorizontal": 10,
-        "paddingHorizontal": 2
+        "paddingHorizontal": 2,
+        "fontFamily": "monospace"
     }
 });

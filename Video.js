217940ef26fcad7cbb6e2078536e522213bea1d1
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-03 00:06:17 +0530

    changed home screen label's font size

--- Video.js
+++ Video.js
@@ -77,6 +77,7 @@ const styles = StyleSheet.create({
         "marginTop": 5,
         "fontWeight": "400",
         "marginHorizontal": 10,
-        "paddingHorizontal": 2
+        "paddingHorizontal": 2,
+        "fontFamily": "monospace"
     }
 });
